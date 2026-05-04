99d3777a1fd6048ed7b0bc0f8a1d5faad6a22c85
Rafael Delmas <121911971+Rafaelmeritello@users.noreply.github.com>
2026-05-04 13:34:47 -0300

    Update permissoes.php

--- application/views/permissoes/permissoes.php
+++ application/views/permissoes/permissoes.php
@@ -46,10 +46,12 @@
       echo '<td>' . $r->nome . '</td>';
       echo '<td>' . date('d/m/Y', strtotime($r->data)) . '</td>';
       echo '<td>' . $situacao . '</td>';
+      if($r->idPermissao) != 1){
       echo '<td>
                                 <a href="' . base_url() . 'index.php/permissoes/editar/' . $r->idPermissao . '" class="btn-nwe3" title="Editar permissões"><i class="bx bx-edit"></i></a>
                                 <a href="#modal-excluir" role="button" data-toggle="modal" permissao="' . $r->idPermissao . '" class="btn-nwe4" title="Desativar Permissão"><i class="bx bx-notification-off" ></i></a>
                               </td>';
+  }
       echo '</tr>';
   } ?>
             </tbody>
