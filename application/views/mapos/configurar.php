a5f841bde57c983f3b65e989ea520c11796152e0
Rafael Delmas <121911971+Rafaelmeritello@users.noreply.github.com>
2026-05-04 13:48:08 -0300

    Update configurar.php

--- application/views/mapos/configurar.php
+++ application/views/mapos/configurar.php
@@ -5,8 +5,15 @@
                 <span class="icon">
                     <i class="fas fa-wrench"></i>
                 </span>
-                <h5>Configurações do Sistema  <?php echo $usuario; ?></h5>
+                <h5>Configurações do Sistema  </h5>
+                            <pre>
+<?php 
+  
+  print_r($this->session->userdata()); 
+?>
+</pre>
             </div>
+
             <ul class="nav nav-tabs">
                 <li class="active"><a data-toggle="tab" href="#home">Gerais</a></li>
                 <li><a data-toggle="tab" href="#menu1">Financeiro</a></li>
