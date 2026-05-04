f75a6e9a115dac28856d59f08ab137432abfa12b
Rafael Delmas <121911971+Rafaelmeritello@users.noreply.github.com>
2026-05-04 16:08:27 -0300

    Update configurar2.php

--- application/views/mapos/configurar2.php
+++ application/views/mapos/configurar2.php
@@ -5,210 +5,88 @@
                 <span class="icon">
                     <i class="fas fa-wrench"></i>
                 </span>
-                <h5>Configurações do Sistema  </h5>
-                      
+                <h5>Configurações do Sistema</h5>
             </div>
-
-            <ul class="nav nav-tabs">
-          <li class="active"><a data-toggle="tab" href="#home">Gerais</a></li>
-                <li><a data-toggle="tab" href="#menu1">Financeiro</a></li>
-               
-     
-                 <?php 
-// Verifica se o usuário tem a permissão ID 1 (Admin)
-if ($this->session->userdata('permissao') == 1) { 
-?>
-    <!-- Estas abas SÓ APARECEM para o Admin -->
-    <!-- <li><a data-toggle="tab" href="#menu6">API</a></li> -->
-    <!-- <li><a data-toggle="tab" href="#menu4">Atualizações</a></li> -->
-<?php 
-} 
-?>
-            </ul>
-            <form action="<?php echo current_url(); ?>" id="formConfigurar" method="post" class="form-horizontal">
-                <div class="widget-content nopadding tab-content">
-                    <?php echo $custom_error; ?>
-                    <!-- Menu Gerais -->
-                    <div id="home" class="tab-pane fade in active">
+            
+            <div class="widget-content nopadding tab-content">
+                <?php echo $custom_error; ?>
+                
+                <form action="<?php echo current_url(); ?>" id="formConfigurar" method="post" class="form-horizontal">
                     
+                    <div class="widget-content">
+                        <!-- TEMA DO SISTEMA -->
                         <div class="control-group">
                             <label for="app_theme" class="control-label">Tema do Sistema</label>
                             <div class="controls">
                                 <select name="app_theme" id="app_theme">
-                                    <option value="default">Escuro</option>
-                                    <option value="white" <?= $configuration['app_theme'] == 'white' ? 'selected' : ''; ?>>Claro</option>
-                                    <option value="puredark" <?= $configuration['app_theme'] == 'puredark' ? 'selected' : ''; ?>>Pure dark</option>
-                                    <option value="darkorange" <?= $configuration['app_theme'] == 'darkorange' ? 'selected' : ''; ?>>Dark orange</option>
-                                    <option value="darkviolet" <?= $configuration['app_theme'] == 'darkviolet' ? 'selected' : ''; ?>>Dark violet</option>
-                                    <option value="whitegreen" <?= $configuration['app_theme'] == 'whitegreen' ? 'selected' : ''; ?>>White green</option>
-                                    <option value="whiteblack" <?= $configuration['app_theme'] == 'whiteblack' ? 'selected' : ''; ?>>White black</option>
+                                    <option value="default" <?= $configuration['app_theme'] == 'default' ? 'selected' : ''; ?>>Claro</option>
+                                    <option value="white" <?= $configuration['app_theme'] == 'white' ? 'selected' : ''; ?>>Neve</option>
+                                    <option value="dark" <?= $configuration['app_theme'] == 'dark' ? 'selected' : ''; ?>>Escuro</option>
                                 </select>
-                                <span class="help-inline">Selecione o tema que que deseja usar no sistema</span>
+                                <span class="help-inline">Selecione o tema que deseja usar no sistema.</span>
                             </div>
                         </div>
 
-                   
-                        <div class="form-actions">
-                            <div class="span8">
-                                <div class="span9">
-                                    <button type="submit" class="button btn btn-primary">
-                                    <span class="button__icon"><i class='bx bx-save'></i></span><span class="button__text2">Salvar Alterações</span></button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- Menu Financeiro -->
-                    <div id="menu1" class="tab-pane fade">
+                        <!-- CONTROLE DE BAIXA RETROATIVA -->
                         <div class="control-group">
                             <label for="control_baixa" class="control-label">Controle de baixa retroativa</label>
                             <div class="controls">
                                 <select name="control_baixa" id="control_baixa">
-                                    <option value="1">Ativar</option>
+                                    <option value="1" <?= $configuration['control_baixa'] == '1' ? 'selected' : ''; ?>>Ativar</option>
                                     <option value="0" <?= $configuration['control_baixa'] == '0' ? 'selected' : ''; ?>>Desativar</option>
                                 </select>
                                 <span class="help-inline">Ativar ou desativar o controle de baixa financeira, com data retroativa.</span>
                             </div>
                         </div>
+
+                        <!-- CONTROLE DE EDIÇÃO DE OS -->
                         <div class="control-group">
                             <label for="control_editos" class="control-label">Controle de edição de OS</label>
                             <div class="controls">
                                 <select name="control_editos" id="control_editos">
-                                    <option value="1" <?= $configuration['control_editos'] == '0' ? 'selected' : ''; ?>>Ativar</option>
+                                    <option value="1" <?= $configuration['control_editos'] == '1' ? 'selected' : ''; ?>>Ativar</option>
                                     <option value="0" <?= $configuration['control_editos'] == '0' ? 'selected' : ''; ?>>Desativar</option>
                                 </select>
                                 <span class="help-inline">Ativar ou desativar a permissão para alterar ou excluir OS faturada e/ou cancelada.</span>
                             </div>
                         </div>
+
+                        <!-- CONTROLE DE EDIÇÃO DE VENDAS -->
                         <div class="control-group">
                             <label for="control_edit_vendas" class="control-label">Controle de edição de Vendas</label>
                             <div class="controls">
                                 <select name="control_edit_vendas" id="control_edit_vendas">
-                                    <option value="1" <?= $configuration['control_edit_vendas'] == '0' ? 'selected' : ''; ?>>Ativar</option>
+                                    <option value="1" <?= $configuration['control_edit_vendas'] == '1' ? 'selected' : ''; ?>>Ativar</option>
                                     <option value="0" <?= $configuration['control_edit_vendas'] == '0' ? 'selected' : ''; ?>>Desativar</option>
                                 </select>
-                                <span class="help-inline">Ativar ou desativar a permissão para alterar ou excluir vendas faturada.</span>
-                            </div>
-                        </div>
-                        <div class="control-group">
-                            <label for="pix_key" class="control-label">Chave Pix para Recebimento de Pagamentos</label>
-                            <div class="controls">
-                                <input type="text" name="pix_key" value="<?= $configuration['pix_key'] ?>">
-                                <span class="help-inline">Chave Pix para Recebimento de Pagamentos</span>
+                                <span class="help-inline">Ativar ou desativar a permissão para alterar ou excluir vendas faturadas.</span>
                             </div>
                         </div>
 
-                     
-                    <!-- Menu E-mail -->
-                    <div id="menu7" class="tab-pane fade">
+                        <!-- CHAVE PIX -->
                         <div class="control-group">
-                            <label for="EMAIL_PROTOCOL" class="control-label">Protocolo de E-mail</label>
+                            <label for="pix_key" class="control-label">Chave Pix para Recebimento de Pagamentos</label>
                             <div class="controls">
-                                <input type="text" name="EMAIL_PROTOCOL" value="<?= $_ENV['EMAIL_PROTOCOL'] ?>" id="EMAIL_PROTOCOL">
-                                <span class="help-inline">Informe o protocolo que será utilizado</span>
-                            </div>
-                        </div>
-                        <div class="control-group">
-                            <label for="EMAIL_SMTP_HOST" class="control-label">Endereço do Host</label>
-                            <div class="controls">
-                                <input type="text" name="EMAIL_SMTP_HOST" value="<?= $_ENV['EMAIL_SMTP_HOST'] ?>" id="EMAIL_SMTP_HOST">
-                                <span class="help-inline">Informe o endereço do host</span>
-                            </div>
-                        </div>
-                        <div class="control-group">
-                            <label for="EMAIL_SMTP_CRYPTO" class="control-label">Tipo de criptografia</label>
-                            <div class="controls">
-                                <select name="EMAIL_SMTP_CRYPTO" id="EMAIL_SMTP_CRYPTO">
-                                    <option value="tls" <?= $_ENV['EMAIL_SMTP_CRYPTO'] == 'tls' ? 'selected' : ''; ?>>tls</option>
-                                    <option value="ssl" <?= $_ENV['EMAIL_SMTP_CRYPTO'] == 'ssl' ? 'selected' : ''; ?>>ssl</option>
-                                </select>
-                                <span class="help-inline">Tipo de criptografia que será utilizada.</span>
-                            </div>
-                        </div>
-                        <div class="control-group">
-                            <label for="EMAIL_SMTP_PORT" class="control-label">Porta</label>
-                            <div class="controls">
-                                <input type="text" name="EMAIL_SMTP_PORT" value="<?= $_ENV['EMAIL_SMTP_PORT'] ?>" id="EMAIL_SMTP_PORT">
-                                <span class="help-inline">Informe a porta que será utilizada.</span>
-                            </div>
-                        </div>
-                        <div class="control-group">
-                            <label for="EMAIL_SMTP_USER" class="control-label">Usuário</label>
-                            <div class="controls">
-                                <input type="text" name="EMAIL_SMTP_USER" value="<?= $_ENV['EMAIL_SMTP_USER'] ?>" id="EMAIL_SMTP_USER">
-                                <span class="help-inline">Informe nome de usuáriodo e-mail.</span>
-                            </div>
-                        </div>
-                        <div class="control-group">
-                            <label for="EMAIL_SMTP_PASS" class="control-label">Senha</label>
-                            <div class="controls">
-                                <input type="password" name="EMAIL_SMTP_PASS" value="<?= $_ENV['EMAIL_SMTP_PASS'] ?>" id="EMAIL_SMTP_PASS">
-                                <span class="help-inline">Informe a senha do e-mail.</span>
-                            </div>
-                        </div>
-                        <div class="form-actions">
-                            <div class="span8">
-                                <div class="span9">
-                                  <button type="submit" class="button btn btn-primary">
-                                  <span class="button__icon"><i class='bx bx-save'></i></span><span class="button__text2">Salvar Alterações</span></button>
-                                </div>
+                                <input type="text" name="pix_key" id="pix_key" value="<?= $configuration['pix_key']; ?>">
+                                <span class="help-inline">Chave Pix para Recebimento de Pagamentos.</span>
                             </div>
                         </div>
                     </div>
-                </div>
-            </form>
+
+                    <!-- BOTÃO DE SALVAR -->
+                    <div class="form-actions">
+                        <div class="span8">
+                            <div class="span9">
+                                <button type="submit" class="button btn btn-primary">
+                                    <span class="button__icon"><i class='bx bx-save'></i></span>
+                                    <span class="button__text2">Salvar Alterações</span>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+
+                </form>
+            </div>
         </div>
     </div>
 </div>
-<!-- Modal -->
-<div id="modal-confirmaratualiza" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <form action="<?php echo base_url() ?>index.php/clientes/excluir" method="post">
-        <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-            <h5 id="myModalLabel">Atualização de sistema</h5>
-        </div>
-        <div class="modal-body">
-            <h5 style="text-align: left">Deseja realmente fazer a atualização de sistema?</h5>
-            <h7 style="text-align: left">Recomendamos que faça um backup antes de prosseguir!</h7>
-            <h7 style="text-align: left"><br>Faça o backup dos seguintes arquivos pois os mesmo serão excluídos:</h7>
-            <h7 style="text-align: left"><br>* ./assets/anexos</h7>
-            <h7 style="text-align: left"><br>* ./assets/arquivos</h7>
-        </div>
-        <div class="modal-footer" style="display:flex;justify-content: center">
-          <button class="button btn btn-mini btn-danger" data-dismiss="modal" aria-hidden="true"><span class="button__icon"><i class='bx bx-x' ></i></span> <span class="button__text2">Cancelar</span></button>
-          <button id="update-mapos" type="button" class="button btn btn-warning"><span class="button__icon"><i class="bx bx-sync"></i></span><span class="button__text2">Atualizar</span></button>
-        </div>
-    </form>
-</div>
-<!-- Modal -->
-<div id="modal-confirmabanco" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <form action="<?php echo base_url() ?>index.php/clientes/excluir" method="post">
-        <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-            <h5 id="myModalLabel">Atualização de sistema</h5>
-        </div>
-        <div class="modal-body">
-            
-          
-            </h7>
-        </div>
-        <div class="modal-footer" style="display:flex;justify-content: center">
-          <button class="button btn btn-mini btn-danger" data-dismiss="modal" aria-hidden="true"><span class="button__icon"><i class='bx bx-x' ></i></span> <span class="button__text2">Cancelar</span></button>
-          <button id="update-database" type="button" class="button btn btn-warning"><span class="button__icon"><i class="bx bx-sync"></i></span><span class="button__text2">Atualizar</span></button>
-        </div>
-    </form>
-</div>
-<script>
-    $('#update-database').click(function() {
-     
-    });
-    $('#update-mapos').click(function() {
-        
-    });
-    $(document).ready(function() {
-        $('#notifica_whats_select').change(function() {
-            if ($(this).val() != "0")
-                document.getElementById("notifica_whats").value += $(this).val();
-            $(this).prop('selectedIndex', 0);
-        });
-    });
-</script>
