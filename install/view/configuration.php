52b5d4271f2fbefb25e44888412b07bec9e2335f
Rafael Delmas <121911971+Rafaelmeritello@users.noreply.github.com>
2026-05-04 11:18:02 -0300

    Update configuration.php

--- install/view/configuration.php
+++ install/view/configuration.php
@@ -7,25 +7,25 @@
       <div class="form-group clearfix">
         <label for="host" class=" col-md-3">Host</label>
         <div class="col-md-9">
-          <input type="text" value="" id="host" name="host" class="form-control" placeholder="Host de Banco de Dados (geralmente localhost)" />
+          <input type="text" value="" id="host" name="host" class="form-control" disabled placeholder="Informação já vinda do ambiente" />
         </div>
       </div>
       <div class="form-group clearfix">
         <label for="dbuser" class=" col-md-3">Usuário</label>
         <div class=" col-md-9">
-          <input type="text" value="" id="dbuser" name="dbuser" class="form-control" autocomplete="off" placeholder="Nome de usuário do banco de dados" />
+          <input type="text" value="" id="dbuser" name="dbuser" class="form-control" disabled autocomplete="off" disabled placeholder="Informação já vinda do ambiente" />
         </div>
       </div>
       <div class="form-group clearfix">
         <label for="dbpassword" class=" col-md-3">Senha</label>
         <div class=" col-md-9">
-          <input type="password" value="" id="dbpassword" name="dbpassword" class="form-control" autocomplete="off" placeholder="Senha do usuário do banco de dados" />
+          <input type="password" value="" id="dbpassword" name="dbpassword" class="form-control" disabled autocomplete="off" placeholder="Informação já vinda do ambiente" />
         </div>
       </div>
       <div class="form-group clearfix">
         <label for="dbname" class=" col-md-3">Banco de Dados</label>
         <div class=" col-md-9">
-          <input type="text" value="" id="dbname" name="dbname" class="form-control" placeholder="Nome do banco de dados" />
+          <input type="text" value="" id="dbname" name="dbname" class="form-control" disabled placeholder="Informação já vinda do ambiente" />
         </div>
       </div>
     </div>
